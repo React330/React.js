269143212546276bac444ab7ff0d58b08ea13ea4
React330 <73373287+React330@users.noreply.github.com>
2022-04-21 13:15:07 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCount = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
@@ -13,6 +31,9 @@ const App = () => {
       <ColorfullMessage color="pink">元気です!</ColorfullMessage>
       <button onClick={onClickCount}>カウント</button>
       <p>{num}</p>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
+      {faceShowFlag && <p>(*'ω'*)</p>}
     </>
   );
 };
